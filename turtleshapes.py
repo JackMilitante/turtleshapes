2c35b2871f01f5400207c8ea0859cc0ca31ee3cf
Jack Militante <jackmilitante10@gmail.com>
2019-11-02 22:02:47 -0500

    change shapes to colored circles

--- turtleshapes.py
+++ turtleshapes.py
@@ -20,8 +20,16 @@ def shape(size,sides):
         turtle.left(360/sides)
 
 for i in range(10000000):
-    shape(i, i)
+    # shape(i, i)
+    # turtle.left(i)
+    turtle.circle(3*i)
+    turtle.circle(-5*i)
     turtle.left(i)
+    b=i
+    if b > 51:
+        b = 51
+    turtle.color(i,2*i,5*b)
+
 
 #wait for user to exit
 turtle.exitonclick()
